941169fc81564f0108872863769e339db5f18fbb
Manh Vu <manh.vuduc@cerebral.com>
2024-10-28 13:58:59 +0700

    pr 3 add error and math

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package calculator
 
+import (
+	"errors"
+	"math"
+)
+
 type Calculator struct{}
 
 func (c Calculator) Add(a, b float64) float64 {
@@ -12,4 +17,15 @@ func (c Calculator) Subtract(a, b float64) float64 {
 
 func (c Calculator) Multiply(a, b float64) float64 {
 	return a * b
+}
+
+func (c Calculator) Divide(a, b float64) (float64, error) {
+	if b == 0 {
+			return 0, errors.New("division by zero is not allowed")
+	}
+	return a / b, nil
+}
+
+func (c Calculator) Power(a, b float64) float64 {
+	return math.Pow(a, b)
 }
